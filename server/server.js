cdc192f12add0d4f550a9addb414796c94a770e0
Sami Rahman <sami1@MacBook-Pro-Sami-2.local>
2023-11-07 14:30:17 -0500

    main server commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Requiring express and CORs to be used
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// access the port variable from the config.env required
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// Get driver connection
+const dbo = require("./db/conn");
+app.listen(port, () => {
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log('Server is running on port: ${port}');
+});
+
